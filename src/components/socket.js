ea747c93f1ac7858ae6fadd35e27f27f7717160d
Samannaway <samannawayghosh@gmail.com>
2024-03-09 21:34:17 +0530

    minor changes

--- src/components/socket.js
+++ src/components/socket.js
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 import { backendHost } from './routes';
 
-const URL = process.env.NODE_ENV === 'production' ? undefined : backendHost;
+const URL = backendHost;
 
 export const socket = io(URL, {
     autoConnect: false
